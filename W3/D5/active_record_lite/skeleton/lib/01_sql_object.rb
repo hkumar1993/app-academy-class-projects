f40b2ae7e2e39bbff0a29bb3cb199a8c3b12dc5c
Harsh <harsh@hkumar.me>
2017-08-18 11:34:23 -0700

    start work on MyActiveRecord, push old unpushed files

---
+++ W3/D5/active_record_lite/skeleton/lib/01_sql_object.rb
@@ -0,0 +1,75 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+
+  def self.columns
+    return @columns if @columns
+    rows = DBConnection.execute2(<<-SQL)
+      SELECT *
+      FROM #{self.table_name}
+    SQL
+    @columns = rows[0].map(&:to_sym)
+  end
+
+  def self.finalize!
+    self.columns
+    @columns.each do |method_symbol|
+      method_name = method_symbol.to_s
+      define_method(method_name) do
+        self.attributes[method_symbol]
+      end
+
+      define_method("#{method_name}=") do |arg|
+        self.attributes[method_symbol] = arg
+      end
+
+    end
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name
+    @table_name ||= self.to_s.tableize
+  end
+
+  def self.all
+    # ...
+  end
+
+  def self.parse_all(results)
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    # ...
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
