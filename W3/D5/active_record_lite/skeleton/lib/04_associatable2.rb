e98da1a72b1aa004b80fb527a967f6b2096b3975
hkumar1993 <harsh@hkumar.me>
2017-08-21 22:06:29 -0700

    complete part 1 of w4d1 projcets

--- W3/D5/active_record_lite/skeleton/lib/04_associatable2.rb
+++ W3/D5/active_record_lite/skeleton/lib/04_associatable2.rb
@@ -5,6 +5,10 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    through_options = assoc_options[through_name]
+    define_method(name) do
+      source_options = through_options.model_class.assoc_options[source_name]
+      self.send(through_name).send(source_name)
+    end
   end
 end
