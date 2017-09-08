79f07a0a9c8af5324e55224f735f7bae5f4619c0
hkumar1993 <harsh@hkumar.me>
2017-09-07 17:25:34 -0700

    Work on Phase 4 : Ajax

--- jquery-lite/lib/jquery_lite.js
+++ jquery-lite/lib/jquery_lite.js
@@ -87,6 +87,37 @@ window.$l = (selector) => {
   }
 };
 
+$l.extend = (...objects) => {
+  return Object.assign({}, ...objects);
+}
+
+$l.ajax = (options) => {
+
+  let defaults = {
+    url: window.location.href,
+    method: 'GET',
+    dataType: 'HTTP',
+    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+    data: {},
+    success: () => {},
+    error: () => {}
+  };
+
+  options = $l.extend(defaults, options);
+  options.method = options.method.toUpperCase();
+
+  const xhr = new XMLHttpRequest();
+  xhr.open(options.method, options.url);
+
+  xhr.onload = function () {
+  console.log(xhr.status) // for status info
+  console.log(xhr.responseType) //the type of data that was returned
+  console.log(xhr.response) //the actual response. For json api calls, this will be a json string
+  }
+
+  xhr.send(options);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   docReady = true;
   queue.forEach((func) => {
